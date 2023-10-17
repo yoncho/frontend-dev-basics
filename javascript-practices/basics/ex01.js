f74b210afecbfefe034c7da53d64eeb2dfab328b
yoncho <joyonghyeon13@gmail.com>
2023-10-17 15:08:40 +0900

    hello js

---
+++ javascript-practices/basics/ex01.js
@@ -0,0 +1,4 @@
+console.log("hello:)");
+console.info("hello:)");
+console.warn("hello:)");
+console.error("hello:)");
---
+++ javascript-practices/basics/ex02.js
@@ -0,0 +1,22 @@
+/**
+ * 변수와 자료형
+ * 
+ * 기본타입
+ *  - undefined
+ *  - number
+ *  - string
+ *  - boolean
+ *  
+ * 객체
+ *  - object
+ *  - function
+ *  오브젝트
+ */
+console.log("=== 기본타입 () ====")
+var u = undefined; // var u; 랑 동일, Java script는 변수 정의와 선언을 구분한다.
+var i = 10;
+// v; //변수이지만 정의되어있지 않음...error...
+
+10; // 레터럴값 그 자체,,
+console.log(u +":"+typeof(u));
+console.log(i +":"+typeof(i));
