6a5964b683a14fab557e4773e2adf308c238d1f1
yoncho <joyonghyeon13@gmail.com>
2023-10-18 15:52:09 +0900

    study - js #object3

--- javascript-practices/basics/ex09.js
+++ javascript-practices/basics/ex09.js
@@ -3,3 +3,26 @@
  * 
  */
 
+var MyObject = function(name, age){
+    this.name = name;
+    this.age = age;
+};
+
+MyObject.prototype.school = 'bitacademy';
+MyObject.prototype.course = 'java fullstack';
+MyObject.prototype.info = function(){
+    console.log(this.name, this.age, this.school, this.course);
+};
+
+//MyObject 인스턴스 생성
+var o1 = new MyObject('dooly', 10);
+o1.info();
+o1.school = 'poscodx'; // 이걸...override라고함... 
+o1.info();
+
+//MyObject 인스턴스 생성2
+var o2 = new MyObject('dooly', 10);
+o2.info = function(){
+    console.log("안 알랴쥼~");
+}
+o2.info();
