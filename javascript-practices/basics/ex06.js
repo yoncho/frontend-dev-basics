4b6544c5eff4f6eb116a96d0c0716459cbba6999
yoncho <joyonghyeon13@gmail.com>
2023-10-18 13:49:33 +0900

    study - js #object

---
+++ javascript-practices/basics/ex06.js
@@ -0,0 +1,8 @@
+/**
+ * javascript 객체1 : Object Type
+ * 
+ * 1. javascript 객체는 function타입과 object타입 딱 두가지 뿐이다.
+ * 2. 보통, function 타입의 객체는 함수라고 부른다.
+ * 3. 따라서, javascript에서 객체라고 부르는것은 object type의 객체를 가리킨다. 
+ * 
+ */
