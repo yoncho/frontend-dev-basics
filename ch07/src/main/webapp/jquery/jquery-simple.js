345676dc0e601f40ff3af3cd80a122aa9eebabd7
yoncho <joyonghyeon13@gmail.com>
2023-10-23 13:46:15 +0900

    study - jquery #3

---
+++ ch07/src/main/webapp/jquery/jquery-simple.js
@@ -0,0 +1,38 @@
+var _jQuery = function(elements){
+	this.length = elements.length;
+	for(var i = 0; i < this.length; i++){
+		this[i] = elements[i];
+	}
+}
+_jQuery.prototype.jquery = '0.0.1.beta';
+
+_jQuery.prototype.click = function(handler){
+	for(var i = 0; i < this.length; i++){
+		this[i].addEventListener('click', handler);
+	}
+	
+	return this;
+};
+
+_jQuery.prototype.css = function(name, value){
+	for(var i = 0; i < this.length; i++){
+		this[i].style[name] = value;
+	}
+	
+	return this;
+}
+
+var jQuery = function(param){
+	if(typeof(param) === 'function'){
+		window.addEventListener('load', param);
+		return;
+	}
+	var elements;
+	if(typeof(param) === 'string'){
+		elements = document.querySelectorAll(param);
+	}
+	
+	return new _jQuery(elements || []); //elements가 빈 배열이면 []를 실행...
+}
+
+var $ = jQuery;
