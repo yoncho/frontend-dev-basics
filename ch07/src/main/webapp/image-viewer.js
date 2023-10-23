528517bd246ca3bacb535b11de83155e1add13b6
yoncho <joyonghyeon13@gmail.com>
2023-10-23 17:41:59 +0900

    study - jquery #image-viewer

--- ch07/src/main/webapp/image-viewer.js
+++ ch07/src/main/webapp/image-viewer.js
@@ -1,6 +1,19 @@
 var imageViewer ={
 	init: function(){
-		
+		var isSlideShow = false;
+		var _this = this;
+		$('#btn-change').click(function(){
+			_this.changeImage();
+		});
+		$('#btn-slideshow').click(function(){
+			isSlideShow = !isSlideShow;
+			$(this).text(isSlideShow ? '슬라이드쇼 중지':'슬라이드쇼 시작');
+			if (isSlideShow){
+				_this.startSlideShow();
+			}else{
+				_this.stopSlideShow();
+			}
+		});
 	},
 	_images : [ 
 		"국화:Chrysanthemum.jpg", 
@@ -11,6 +24,23 @@ var imageViewer ={
 		"등대:Lighthouse.jpg", 
 		"펭귄:Penguins.jpg", 
 		"툴립:Tulips.jpg" 
-	]
-	
+	],
+	changeImage: function(){
+		var index = Math.floor( Math.random() * (this._images.length-1)  ) + 1;
+		var info = this._images[index].split(":");
+		
+		$('#image').attr("src", '/ch07/images/' + info[1]);
+		$('#image').attr("alt", info[0]);
+		console.log(info[0], info[1]);
+	},
+	slideShow:null,
+	startSlideShow: function(){
+		var _this = this;
+		this.slideShow = setInterval(function(){
+			_this.changeImage();
+		}, 1000);		
+	},
+	stopSlideShow: function(){
+		clearInterval(this.slideShow);
+	}
 }
